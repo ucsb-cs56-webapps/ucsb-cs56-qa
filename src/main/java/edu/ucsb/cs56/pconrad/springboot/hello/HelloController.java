5efa762cec6ab353445a7e8ebdbf6dc288be92b0
Ken Xu <33238360+ChangweiXu@users.noreply.github.com>
2018-12-01 08:26:33 -0800

    KX: delete test template: test.ftl

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
@@ -48,42 +48,6 @@ public class HelloController {
     }
 
     // DEBUG
-    @RequestMapping("/test")
-    public String test() {
-        /* DEBUG
-        // DatabaseAPI.saveDataDemo();
-        // DatabaseAPI.readDataDemo();
-
-        // User a = new User("u1","u1@ucsb.edu","u001","a0a0a0");
-        // DatabaseAPI.createUser(a);
-        // User b = new User("u2","u2@ucsb.edu","u002","b0b0b0");
-        // DatabaseAPI.createUser(b);
-
-        // a = DatabaseAPI.findUser("u002");
-        // b = DatabaseAPI.findUser("u001");
-
-        // Answer a = new Answer("2018:11:30:10:18:15_u002","content01","u001");
-        // Answer b = new Answer("2018:11:30:10:18:15_u001","content02","u002");
-
-        // DatabaseAPI.composeAnswer(a);
-        // DatabaseAPI.composeAnswer(b);
-
-        // System.out.println(DatabaseAPI.retrieveUserQuestionList("u001"));
-        // System.out.println(DatabaseAPI.retrieveUserQuestionList("u002"));
-
-        System.out.println(DatabaseAPI.findAnswer("2018:11:30:11:59:24_u002"));
-
-        System.out.println(DatabaseAPI.retrieveUserAnswerList("u001"));
-
-        System.out.println(DatabaseAPI.retrieveQuestionAnswerList("2018:11:30:10:18:15_u001"));
-        */
-        // System.out.println(DatabaseAPI.retrieveQuestionList());
-        return "test";
-    }
-
-
-
-    // DEBUG
     @RequestMapping(value = "/create_user", method = RequestMethod.GET)
     public ModelAndView testCreateUser() {
         return new ModelAndView("testCreateUser", "user", new User());
@@ -105,8 +69,6 @@ public class HelloController {
 
         return "redirect:/testuid=" + user.getUserid();
     }
-
-
 
     // DEBUG
     @RequestMapping(value="/testuid={uid}", method = RequestMethod.GET)
